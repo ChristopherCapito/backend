7fdf8d4598d4f44708e54e989125bee914cb5ae7
ChristopherCapito <christopher@capito.rocks>
2020-11-11 03:40:45 +0100

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd84e0534330fb30f5cd9b73e44ea9096'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e58487be-9718-4a0a-a7ce-22311868055c'
+};
