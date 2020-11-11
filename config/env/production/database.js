be88e51d047a6f65d1360c87cbec2175f8e6754c
ChristopherCapito <christopher@capito.rocks>
2020-11-11 05:20:28 +0100

    Trying to fix the port issue

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,42 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: "bookshelf",
+      connector: 'bookshelf',
       settings: {
-        client: "postgres",
-        host: "${process.env.DATABASE_HOST}",
-        port: "${process.env.DATABASE_PORT}",
-        database: "${process.env.DATABASE_NAME}",
-        username: "${process.env.DATABASE_USERNAME}",
-        password: "${process.env.DATABASE_PASSWORD}",
-        ssl: { "rejectUnauthorized": false }
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
       },
-      options: {}
-    }
-  }
+      options: {
+        ssl: false,
+      },
+    },
+  },
 });
+
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: "bookshelf",
+//       settings: {
+//         client: "postgres",
+//         host: "${process.env.DATABASE_HOST}",
+//         port: "${process.env.DATABASE_PORT}",
+//         database: "${process.env.DATABASE_NAME}",
+//         username: "${process.env.DATABASE_USERNAME}",
+//         password: "${process.env.DATABASE_PASSWORD}",
+//         ssl: { "rejectUnauthorized": false }
+//       },
+//       options: {}
+//     }
+//   }
+// });
